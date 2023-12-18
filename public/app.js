821931b8f5cb2ed5fb7a856fadaf8dfa41a2f319
zulvaaraf <zulvasebelas@gmail.com>
2023-12-18 17:07:34 +0700

    inline style with react

--- public/app.js
+++ public/app.js
@@ -1,53 +1,11 @@
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-function MyButton({
-  text,
-  onClick,
-  counter
-}) {
-  const myStyle = text !== 'Reset!!!' ? {} : {
-    margin: 20,
-    display: 'block'
-  };
-  return /*#__PURE__*/React.createElement("button", {
-    onClick: onClick,
-    style: myStyle
-  }, text);
-}
-function MyCounter({
-  counter
-}) {
-  return /*#__PURE__*/React.createElement("span", {
-    style: {
-      margin: 20
-    }
-  }, counter);
-}
-function HomePage() {
-  const [counter, setCounter] = React.useState(0);
-  function increment() {
-    setCounter(counter + 1);
+const element = /*#__PURE__*/React.createElement("div", {
+  style: {
+    marginLeft: 200,
+    width: 200,
+    height: 200,
+    backgroundColor: 'blue'
   }
-  function decrement() {
-    setCounter(counter - 1);
-  }
-  function reset() {
-    setCounter(0);
-  }
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(MyButton, {
-    text: '-',
-    onClick: decrement,
-    counter: counter
-  }), /*#__PURE__*/React.createElement(MyCounter, {
-    counter: counter
-  }), /*#__PURE__*/React.createElement(MyButton, {
-    text: '+',
-    onClick: increment,
-    counter: counter
-  }), /*#__PURE__*/React.createElement(MyButton, {
-    text: 'Reset!!!',
-    onClick: reset,
-    counter: counter
-  }));
-}
-root.render( /*#__PURE__*/React.createElement(HomePage, null));
+});
+root.render(element);
--- src/app.js
+++ src/app.js
@@ -1,43 +1,15 @@
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-function MyButton({ text, onClick, counter }) {
-  const myStyle = text !== 'Reset!!!' ? {} : { margin: 20, display: 'block' };
+const element = (
+  <div
+    style={{
+      marginLeft: 200,
+      width: 200,
+      height: 200,
+      backgroundColor: 'blue',
+    }}
+  ></div>
+);
 
-  return (
-    <button onClick={onClick} style={myStyle}>
-      {text}
-    </button>
-  );
-}
-
-function MyCounter({ counter }) {
-  return <span style={{ margin: 20 }}>{counter}</span>;
-}
-
-function HomePage() {
-  const [counter, setCounter] = React.useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
-  function decrement() {
-    setCounter(counter - 1);
-  }
-
-  function reset() {
-    setCounter(0);
-  }
-
-  return (
-    <>
-      <MyButton text={'-'} onClick={decrement} counter={counter} />
-      <MyCounter counter={counter} />
-      <MyButton text={'+'} onClick={increment} counter={counter} />
-      <MyButton text={'Reset!!!'} onClick={reset} counter={counter} />
-    </>
-  );
-}
-
-root.render(<HomePage />);
+root.render(element);
