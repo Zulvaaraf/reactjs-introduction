c1a5ff18594e5c5d987c28d6789e725f5e62ccbe
zulvaaraf <zulvasebelas@gmail.com>
2023-12-20 14:29:32 +0700

    component life sycle useEffect

--- public/app.js
+++ public/app.js
@@ -1,7 +1,21 @@
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-const boxColor = 'blue';
-const element = /*#__PURE__*/React.createElement("div", {
-  className: boxColor
-});
-root.render(element);
+function HomePage() {
+  const [click, setClick] = React.useState(false);
+  const [count, setCount] = React.useState(0);
+  React.useEffect(() => {
+    console.log(document.getElementById('judul'));
+  }, [count]);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
+    id: "judul"
+  }, "Hallo ini halaman utama"), /*#__PURE__*/React.createElement("button", {
+    onClick: () => {
+      setClick(true);
+    }
+  }, "klik aku dong"), /*#__PURE__*/React.createElement("button", {
+    onClick: () => {
+      setCount(count + 1);
+    }
+  }, "Tambah"), "Nilai Saat Ini: ", count);
+}
+root.render( /*#__PURE__*/React.createElement(HomePage, null));
--- src/app.js
+++ src/app.js
@@ -1,8 +1,34 @@
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-const boxColor = 'blue';
+function HomePage() {
+  const [click, setClick] = React.useState(false);
+  const [count, setCount] = React.useState(0);
 
-const element = <div className={boxColor}></div>;
+  React.useEffect(() => {
+    console.log(document.getElementById('judul'));
+  }, [count]);
 
-root.render(element);
+  return (
+    <>
+      <h1 id="judul">Hallo ini halaman utama</h1>
+      <button
+        onClick={() => {
+          setClick(true);
+        }}
+      >
+        klik aku dong
+      </button>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Tambah
+      </button>
+      Nilai Saat Ini: {count}
+    </>
+  );
+}
+
+root.render(<HomePage />);
