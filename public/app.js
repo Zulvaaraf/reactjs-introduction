ae47e61fb820de4018e4aa38520b2e548ef1f0db
zulvaaraf <zulvasebelas@gmail.com>
2023-12-21 20:36:19 +0700

    sinkronisasi data fetching dengan UI

--- public/app.js
+++ public/app.js
@@ -1,15 +1,21 @@
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 function App() {
-  React.useEffect(function () {
+  const [news, setNews] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  React.useEffect(() => {
     async function getData() {
       const request = await fetch('https://api.spaceflightnewsapi.net/v3/blogs');
-      console.log(request);
       const response = await request.json();
-      console.log(response);
+      setNews(response);
+      setLoading(false);
     }
     getData();
   }, []);
-  return /*#__PURE__*/React.createElement("h1", null, "Data fetch");
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Data Fetch"), loading ? /*#__PURE__*/React.createElement("p", null, "Loading...") : /*#__PURE__*/React.createElement("ul", null, news.map(item => {
+    return /*#__PURE__*/React.createElement("li", {
+      key: item.id
+    }, item.title);
+  })));
 }
 root.render( /*#__PURE__*/React.createElement(App, null));
--- src/app.js
+++ src/app.js
@@ -2,18 +2,34 @@ const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 function App() {
-  React.useEffect(function () {
+  const [news, setNews] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
     async function getData() {
       const request = await fetch('https://api.spaceflightnewsapi.net/v3/blogs');
-      console.log(request);
 
       const response = await request.json();
-      console.log(response);
+      setNews(response);
+      setLoading(false);
     }
     getData();
   }, []);
 
-  return <h1>Data fetch</h1>;
+  return (
+    <>
+      <h1>Data Fetch</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {news.map((item) => {
+            return <li key={item.id}>{item.title}</li>;
+          })}
+        </ul>
+      )}
+    </>
+  );
 }
 
 root.render(<App />);
